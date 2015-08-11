de48d018102f083b971f383ff4d5f2198b049cd5
Orest Prystayko <orest.prystayko@eleks.com>
2015-08-11 18:07:29 +0300

    styles, logic, layout

--- app.js
+++ app.js
@@ -5,42 +5,57 @@ filter by parentId
 ...
  */
 let treeContainer = document.querySelector('#tree-container');
-const rawTree = fetch('./source.json');
-rawTree.then(function(res) {
-    res.json().then(function(json) {
+
+const rawTree = fetch('http://eleks-treeview.rhcloud.com');
+const activeNode = fetch('http://eleks-treeview.rhcloud.com/activeNodeId');
+
+Promise.all([rawTree, activeNode]).then(function(res) {
+    Promise.all([res[0].json(), res[1].json()]).then(function(res) {
+        const jsonTree = res[0];
+        const activeNodeId = res[1].activeNodeId;
+
         let tree = {};
 
-        json.forEach(function(el) { //create hash with id? {_id...:{}, ...}
+        jsonTree.forEach(function(el) { //create hash with id
             tree[el._id] = {
                 name: el.name,
                 _id: el._id,
                 parentId: el.parentId,
                 //add extra props
                 state: {
-                    opened: false //TODO
+                    opened: activeNodeId === el._id,
+                    folder: false
                 },
                 childens: []
-            }
+            };
         });
-
 
         Object.keys(tree).forEach(function(key, i) {
             const parentId = tree[key].parentId,
-                elem = tree[key];
+                elem = tree[key],
+                isOpened = elem.state.opened;
             if (parentId) {
                 tree[parentId].childens.push(elem);
                 elem.state.ordered = true;
+            } else {
+                elem.state.opened = true; //for root elems
             }
-        })
+            /*if (isOpened && parentId) {
+               tree[parentId].state.opened = true;
+            }*/
+        });
 
         let filteredTreeArr = [];
         Object.keys(tree).forEach(function(key, i) {
-            if (tree[key].state.ordered) {
-                delete tree[key];
-            } else {
-                filteredTreeArr.push(tree[key])
+            if (tree[key].childens.length) {
+                tree[key].state.folder = true;
             }
-        })
+            if (tree[key].state.ordered) {
+                delete tree[key]; //dublicates
+            } else {
+                filteredTreeArr.push(tree[key]);
+            }
+        });
 
         function buildTree(treeArr, parent) {
             let ul = document.createElement('UL');
@@ -56,15 +71,41 @@ rawTree.then(function(res) {
 
                 ul.appendChild(li);
 
+                if (!el.state.opened) {
+                    li.hidden = true;
+                }
+
                 if (el.childens.length) {
-                    buildTree(el.childens, li)
+                    buildTree(el.childens, li);
                 }
             });
             if (!parent) {
-                treeContainer.appendChild(ul)
+                treeContainer.appendChild(ul);
             }
         }
 
+        initState(filteredTreeArr);
         buildTree(filteredTreeArr);
+
+        function initState(treeArr, parent) {
+            treeArr.forEach(function(el) {
+                if (el._id === activeNodeId) {
+                    el.state.opened = true;
+                    el.childens && el.childens.forEach(function(el) {
+                        el.state.opened = true;
+                    });
+                    if (parent) {
+                        parent.state.opened = true;
+                        /*parent.childens.forEach(function(el) {
+                            el.state.opened = true;
+                        });*/
+                    }
+                } else {
+                    el.childens && initState(el.childens, el);
+                }
+            });
+        }
+
     });
+
 });
--- js-treeview-master/src/treeview.js
+++ js-treeview-master/src/treeview.js
@@ -1,8 +1,5 @@
 (function(define) {
     'use strict';
-
-
-
     /** List of events supported by the tree view */
     var events = ['expand', 'collapse', 'select'];
 
@@ -12,11 +9,23 @@
      * @property {object} data The JSON object that represents the tree structure
      * @property {DOMElement} node The DOM element to render the tree in
      */
-    function TreeView(data, node) {
+    function TreeView(urls, node, inputEl) {
         this.handlers = {};
         this.node = node;
-        this.data = data;
-        render(this);
+
+
+        let promises = [];
+        urls.forEach(function(url) {
+            promises.push(httpGet(url));
+        });
+        let self = this;
+        Promise.all(promises).then(function(jsons) {
+            self.rawData = JSON.parse(jsons[0]);
+            self.activeNode = JSON.parse(jsons[1]);
+            self.data = createGenericTree(self.rawData, self.activeNode);
+            render(self);
+            initSearch(inputEl);
+        });
     }
 
     /**
@@ -29,6 +38,21 @@
         for (i = 0; i < len; i += 1) {
             callback(arr[i], i);
         }
+    }
+
+    function initSearch(inputEl) {
+        document.querySelector(inputEl).onkeyup = function() {
+            var value = this.value;
+            if (value.length > 2) {
+                [].slice.call(document.querySelectorAll('#tree a')).forEach(function(node) {
+                    if (node.textContent.toLowerCase().slice(0, -1) === value) {
+                        node.classList.add('highlighted');
+                    } else {
+                        node.classList.remove('highlighted');
+                    }
+                });
+            }
+        };
     }
 
     function isOpened(item) {
@@ -45,6 +69,72 @@
         return state;
     }
 
+    function httpGet(url) {
+
+        return new Promise(function(resolve, reject) {
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', url, true);
+
+            xhr.onload = function() {
+                if (this.status == 200) {
+                    resolve(this.response);
+                } else {
+                    var error = new Error(this.statusText);
+                    error.code = this.status;
+                    reject(error);
+                }
+            };
+
+            xhr.onerror = function() {
+                reject(new Error("Network Error"));
+            };
+
+            xhr.send();
+        });
+
+    }
+
+    function createGenericTree(rawData, activeNode) {
+        let tree = {};
+        rawData.forEach(function(el) { //create hash with id
+            tree[el._id] = {
+                name: el.name,
+                _id: el._id,
+                parentId: el.parentId,
+                //add extra props
+                state: {
+                    opened: activeNode.activeNodeId === el._id,
+                    folder: false
+                },
+                children: []
+            };
+        });
+
+        Object.keys(tree).forEach(function(key, i) {
+            const elem = tree[key],
+                parentId = elem.parentId;
+            if (parentId) {
+                tree[parentId].children.push(elem);
+                elem.state.ordered = true;
+            }
+        });
+
+        let genericTree = [];
+        Object.keys(tree).forEach(function(key, i) {
+            if (tree[key].children.length) {
+                tree[key].state.folder = true;
+            }
+            if (tree[key].state.ordered) {
+                delete tree[key]; //dublicates
+            } else {
+                genericTree.push(tree[key]);
+            }
+        });
+
+        return genericTree;
+    }
+
     /**
      * Renders the tree view in the DOM
      */
@@ -53,29 +143,31 @@
         var leaves = [],
             click;
         var renderLeaf = function(item) {
-            var leaf = document.createElement('div');
-            var content = document.createElement('div');
-            var text = document.createElement('div');
+            var leaf = document.createElement('li');
+            var content = document.createElement('a');
+            //var text = document.createElement('div');
             var expando = document.createElement('div');
 
             leaf.setAttribute('class', 'tree-leaf');
+            if (isOpened(item)) {
+                leaf.classList.add('expanded');
+                if (item.name === 'Apple') {
+                    leaf.classList.add('active');
+                };
+            }
             content.setAttribute('class', 'tree-leaf-content');
             content.setAttribute('data-item', JSON.stringify(item));
-            text.setAttribute('class', 'tree-leaf-text');
-            text.textContent = item.name;
+            content.textContent = item.name;
             expando.setAttribute('class', 'tree-expando expanded');
             expando.textContent = '-';
             content.appendChild(expando);
-            content.appendChild(text);
             leaf.appendChild(content);
 
-            console.log(isOpened(item));
-
             if (item.children.length > 0) {
-                var children = document.createElement('div');
+                var children = document.createElement('UL');
                 children.setAttribute('class', 'tree-child-leaves');
                 if (!isOpened(item)) {
-                    expando.textContent = '+'
+                    expando.textContent = '+';
                     children.classList.add('hidden');
                 }
                 forEach(item.children, function(child) {
@@ -92,9 +184,13 @@
         forEach(self.data, function(item) {
             leaves.push(renderLeaf(item));
         });
-        container.innerHTML = leaves.map(function(leaf) {
+        var rootUl = document.createElement('UL');
+        rootUl.classList.add('tree');
+        rootUl.innerHTML = leaves.map(function(leaf) {
             return leaf.outerHTML;
         }).join('');
+
+        container.appendChild(rootUl);
 
         click = function(e) {
             var parent = (e.target || e.currentTarget).parentNode;
